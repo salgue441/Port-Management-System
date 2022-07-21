e61af9a441fa1c033cbfb0531431ed3ba6f1ab25
salgue441 <carlossalguero441@gmail.com>
2022-07-20 22:38:42 -0600

    Updates in code

--- headers/refrigerated_container.h
+++ headers/refrigerated_container.h
@@ -16,11 +16,12 @@ public:
     std::string show_container_data();
 
 private:
-    float refrigeration_rate{0.0};
-    double gas_consumption{0.0};
-    int max_time{96};
+    float refrigeration_rate{15.14};
+    double gas_consumption_hr{16.5}, max_gas{200}, current_gas{0.0};
+    int max_time{250};
 
     /* ---- Functions ---- */
+    int calculate_times_passed() const;
     double calc_refrigeration_costs() const;
     double calc_gas_consumption() const;
     double calc_cost() const;
